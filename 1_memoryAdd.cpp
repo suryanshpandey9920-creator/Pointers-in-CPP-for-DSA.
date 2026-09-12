99bfc164e4fe21d7fa4871881a13131c6de80ab1
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-09-12 12:35:56 +0530

    Memory adress Operator in CPP.

---
+++ 1_memoryAdd.cpp
@@ -0,0 +1,10 @@
+// This program is created to show the demonstration of memory addressing in CPP
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int b = 23;
+    cout << "The memory address of " << b  << " is : " << & b << endl;
+    return 0;
+}
