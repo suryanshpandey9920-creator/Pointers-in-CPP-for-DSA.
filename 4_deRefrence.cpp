41f94d018fb908ee2888585b0c48f45f357a9caa
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-09-15 00:18:27 +0530

    De Refrence Operator in CPP.

--- 4_deRefrence.cpp
+++ 4_deRefrence.cpp
@@ -9,10 +9,13 @@ int main() {
     cin >> a;
 
     int *ptr = &a ;
-
     cout << "Value of a = " << a << "\n";
+
+    // Defrence operator used here and changed the value of a 
+    *ptr = 34;
+
     cout << "memory address of a = " << ptr << "\n";
-    cout << "Defrence operator used here : " << *ptr << "\n";
+    cout << "Value of a after using deRefrence operator : " << *ptr << "\n";
     // the <<  *ptr ; works as the derefrence operator 
     
 
